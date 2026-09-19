16bfaca5ac6cae61ce9cda97048bc873f84f02c0
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-09-20 00:15:40 +0900

    feat: JPA Timestamp 기능을 위한 EnableJpaAuditing 추가

--- src/main/java/com/todo/global/jpa/entity/BaseIdAndTime.java
+++ src/main/java/com/todo/global/jpa/entity/BaseIdAndTime.java
@@ -1,15 +1,18 @@
 package com.todo.global.jpa.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseIdAndTime extends BaseEntity{
 
     @Id
